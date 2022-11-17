212eef4efda6f8fb8d969275caf0f520e930f723
soyvillareal <soyvillareal@gmail.com>
2022-11-16 21:55:30 -0500

    Removing info.php of .gitignore

---
+++ assets/includes/info.php
@@ -0,0 +1,10 @@
+<?php
+$mysql_hostname = "localhost"; // Server name
+$mysql_username = "root"; // Database user
+$mysql_password = ""; // Database password
+$mysql_database = "php-magazine"; // Database name
+
+
+$domain = 'localhost'; // Web domain, eg. (localhost, IP or example.tld)
+$site_url = "http://{$domain}/PHP-Magazine"; // URL and subfolders of the website, eg. (http://${domain}/ or http//${domain}/subfolder). You can also use HTTP or HTTPS.
+?>
